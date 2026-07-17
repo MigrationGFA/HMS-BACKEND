8920da1b3828636ab3bcf4580d5f40ad9825c3c0
YemiAbdulAzeez <adeleyeasquare@gmail.com>
2026-07-17 05:09:43 +0100

    nursing module

--- apps/api/src/pharmacy/pharmacy.module.ts
+++ apps/api/src/pharmacy/pharmacy.module.ts
@@ -1,9 +1,6 @@
 import { Module } from '@nestjs/common';
-<<<<<<< HEAD
 import { NursingModule } from '../nursing/nursing.module';
-=======
 import { AuditModule } from '../audit/audit.module';
->>>>>>> 6f243d98c7656163b07dfc15a488a1f9f189119a
 import { PharmacyController } from './pharmacy.controller';
 import { DispensingController } from './dispensing.controller';
 import { PharmacyInventoryController } from './pharmacy-inventory.controller';
@@ -16,26 +13,16 @@ import { DrugsService } from './drugs.service';
 import { ProcurementService } from './procurement.service';
 
 @Module({
-<<<<<<< HEAD
-  imports: [NursingModule],
-=======
-  imports: [AuditModule],
->>>>>>> 6f243d98c7656163b07dfc15a488a1f9f189119a
+  imports: [NursingModule, AuditModule],
   controllers: [
     PharmacyController,
     DispensingController,
     PharmacyInventoryController,
-<<<<<<< HEAD
-  ],
-  providers: [PharmacyService],
-  exports: [PharmacyService],
-=======
     SuppliersController,
     DrugsController,
     PharmacyProcurementController,
   ],
   providers: [PharmacyService, SuppliersService, DrugsService, ProcurementService],
-  exports: [PharmacyService, SuppliersService, DrugsService, ProcurementService],
->>>>>>> 6f243d98c7656163b07dfc15a488a1f9f189119a
+  exports: [PharmacyService, DrugsService],
 })
 export class PharmacyModule {}
